f36b7d4db4aa3b73e4adfdff826d40bf0b5cc28f
문영식 <moonyoungsik95@gmail.com>
2025-05-08 18:37:54 +0900

    UserDetails, UserDetailsService

--- src/main/java/com/example/SpringJWT/repository/UserRepository.java
+++ src/main/java/com/example/SpringJWT/repository/UserRepository.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
-  UserEntity findByUsername(String username);
-
   Boolean existsByUsername(String username);
+
+  UserEntity findByUsername(String username);
 
 }
